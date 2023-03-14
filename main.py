c4c4293e324eee4b9c7504a33b08569c49c7d9a5
JakubBaczewskiHrmm <115699782+JakubBaczewskiHrmm@users.noreply.github.com>
2023-03-14 15:44:44 +0100

    Add files via upload
    Zadania od 1 do 13

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
-import sys
+import math
+# zadanie1
+print("||Zad1 =================================||")
+print(math.pow(math.e,10))
+print(math.pow(math.log(5+math.pow(math.sin(8),2),math.e),(1/6)))
+print(math.floor(3.55))
+print(math.floor(4.80))
 
-print(sys.version)
+# zadanie2
+print("||Zad2 =================================||")
+imie = "JAKUB"
+nazwisko = "BACZEWSKI"
+print(imie.capitalize(),nazwisko.capitalize())
 
+# zadanie3
+print("||Zad3 =================================||")
+tekstPiosenki = "NANIMO NANIMO madashiraru"
+print(tekstPiosenki.count("NANIMO"))
 
+# zadanie4
+print("||Zad4 =================================||")
+lancuchoweSlowa = "ŁAŃCUSZEK"
+print(lancuchoweSlowa[0],lancuchoweSlowa[8])
+
+# zadanie5
+print("||Zad5 =================================||")
+losoweSlowa = "jakieś tam napisy śmieszne podzielone"
+print(losoweSlowa.split())
+
+# zadanie6
+print("||Zad6 =================================||")
+a = "txt"
+b = float(37.17)
+c = 320
+print("%s" % a)
+print("%f" % b)
+print(hex(c))
+
+# zadanie7
+print("||Zad7 =================================||")
+favSportsList = ["siatkówka","bieganie","pływanie","skoki w dal"]
+print(favSportsList)
+favSportsList.reverse()
+favSportsList.append("Biatlon")
+favSportsList.append("Jazda konna")
+print(favSportsList)
+
+# zadanie8
+print("||Zad8 =================================||")
